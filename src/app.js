50c62ef48425d3c2cc821f7b484caa3e8ebfe878
Kartik <9415kks@gmail.com>
2021-09-11 19:46:48 +0530

    Second Commit

--- src/app.js
+++ src/app.js
@@ -7,11 +7,8 @@ const port = process.env.PORT || 3000;
 // console.log(path.join(__dirname,"../public/"));
 
 const static_path = path.join(__dirname,"../public");
-
 app.use(express.static(static_path));
-
 // routing
-
 app.get("",(req,res)=>{
   res.send("Welcome kartik");
 })
